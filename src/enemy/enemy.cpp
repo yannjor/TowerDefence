3f694403a5968e2798cc42f72c9e657183a96cb8
Isac Sund <isac.sund@outlook.com>
2019-11-13 11:03:48 +0200

    Change enemy position to floats

--- src/enemy/enemy.cpp
+++ src/enemy/enemy.cpp
@@ -1,13 +1,13 @@
 #include "enemy.hpp"
 
-Enemy::Enemy(int hp, int speed, int x, int y, const std::string& texture,
+Enemy::Enemy(int hp, int speed, float x, float y, const std::string& texture,
              EnemyTypes type)
     : hp_(hp), speed_(speed), x_(x), y_(y), texture_(texture), type_(type) {}
 
 void Enemy::Move() {}  // TODO: Implement A* search algorithm xd
 const int Enemy::GetHp() const { return hp_; }
 const int Enemy::GetSpeed() const { return speed_; }
-const std::pair<int, int> Enemy::GetPosition() const { return {x_, y_}; }
+const std::pair<float, float> Enemy::GetPosition() const { return {x_, y_}; }
 const std::string& Enemy::GetTexture() const { return texture_; }
 const EnemyTypes Enemy::GetType() const { return type_; }
 const bool Enemy::IsAlive() const { return hp_ > 0; }
--- src/enemy/enemy.hpp
+++ src/enemy/enemy.hpp
@@ -4,13 +4,13 @@ enum EnemyTypes { Standard, Fast /*, Big, Boss*/ };
 
 class Enemy {
  public:
-  Enemy(int hp, int speed, int x, int y,
+  Enemy(int hp, int speed, float x, float y,
         const std::string& texture = "sprites/basic_enemy.png",
         EnemyTypes type = Standard);
   void Move();
   const int GetHp() const;
   const int GetSpeed() const;
-  const std::pair<int, int> GetPosition() const;
+  const std::pair<float, float> GetPosition() const;
   const std::string& GetTexture() const;
   const EnemyTypes GetType() const;
   const bool IsAlive() const;
@@ -19,7 +19,7 @@ class Enemy {
  private:
   int hp_;
   int speed_;
-  int x_, y_;
+  float x_, y_;
   const std::string texture_;
   EnemyTypes type_;
 };
