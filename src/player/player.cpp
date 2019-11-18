1ba6c59611483c834dc4da74bdc8fd3a8c45539a
Markuus <markus.henttonen@aalto.fi>
2019-11-18 11:23:55 +0200

    Add lives

--- src/player/player.cpp
+++ src/player/player.cpp
@@ -1,9 +1,11 @@
 #include "player.hpp"
 
-Player::Player(const std::string& name, int money, int score)
-    : money_(money), score_(score), name_(name) {}
+Player::Player(const std::string& name, int lives = 5, int money, int score)
+    : money_(money), score_(score), name_(name), lives_(lives) {}
 int Player::GetMoney() const { return money_; }
 int Player::GetScore() const { return score_; }
+int Player::GetLives() const { return lives_; }
 const std::string& Player::GetName() const { return name_; }
 void Player::AddMoney(int money) { money_ += money; }
 void Player::AddScore(int score) { score_ += score; }
+void Player::RemoveLives(int lives) { lives_ -= lives; }
--- src/player/player.hpp
+++ src/player/player.hpp
@@ -2,15 +2,19 @@
 
 class Player {
  public:
-  Player(const std::string& name, int money = 500, int score = 0);
+  Player(const std::string& name, int lives = 10, int money = 500,
+         int score = 0);
   int GetMoney() const;
   int GetScore() const;
+  int GetLives() const;
   const std::string& GetName() const;
   void AddMoney(int money);
   void AddScore(int score);
+  void RemoveLives(int damage);
 
  private:
   int money_;
   int score_;
+  int lives_;
   const std::string name_;
 };
