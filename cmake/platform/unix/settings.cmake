05aebcf3c172368022b1c3ab3144fc8c1e4668be
Isac Sund <isac.sund@outlook.com>
2019-11-20 12:29:15 +0200

    Add platform checks for compiler version

---
+++ cmake/platform/unix/settings.cmake
@@ -0,0 +1,6 @@
+message(STATUS "UNIX: Detected compiler: ${CMAKE_C_COMPILER}")
+if(CMAKE_C_COMPILER MATCHES "gcc" OR CMAKE_C_COMPILER_ID STREQUAL "GNU")
+  include(${CMAKE_SOURCE_DIR}/cmake/compiler/gcc/settings.cmake)
+else()
+  add_definitions(-D_BUILD_DIRECTIVE='"${CMAKE_BUILD_TYPE}"')
+endif()
