654f4a5718fc51b4da41a1d430357095d52834dd
Isac Sund <isac.sund@outlook.com>
2019-12-04 20:10:37 +0200

    Fix warnings in Gui class

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -8,11 +8,11 @@ void Gui::Add(const std::string& name, GuiEntry entry) {
   entries_.insert({name, entry});
 }
 
-GuiEntry& Gui::Get(const std::string& name) { entries_.at(name); }
+GuiEntry& Gui::Get(const std::string& name) { return entries_.at(name); }
 
 void Gui::draw(sf::RenderTarget& target, sf::RenderStates states) const {
   if (!visible_) return;
   for (auto entry : entries_) {
-    target.draw(entry.second);
+    target.draw(entry.second, states);
   }
 }
