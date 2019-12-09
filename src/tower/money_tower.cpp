356b43cfa9c49fb685b88c8f50fb301cfcc9a948
Markuus <markus.henttonen@aalto.fi>
2019-12-09 10:32:16 +0200

    Money tower

--- src/tower/money_tower.cpp
+++ src/tower/money_tower.cpp
@@ -4,12 +4,28 @@ MoneyTower::MoneyTower(int x, int y, float size, int price,
                        const std::string& texturename, int money_per_wave)
     : Tower(0, 0, 0, x, y, size, price, texturename),
       money_per_wave_(money_per_wave) {
-  max_upgrade_ = 3;
+  max_upgrade_ = 4;
+  upgrade_price_ = 100;
 }
 
 void MoneyTower::Upgrade() {
   if (current_upgrade_ < max_upgrade_) {
     current_upgrade_ += 1;
-    money_per_wave_ += 300;
+    switch (current_upgrade_) {
+      case 2:
+        money_per_wave_ += 50;
+        upgrade_price_ += 50;
+        break;
+      case 3:
+        money_per_wave_ += 50;
+        upgrade_price_ += 50;
+        break;
+      case 4:
+        money_per_wave_ += 50;
+        upgrade_price_ += 100;
+        break;
+      default:
+        break;
+    }
   }
 }
--- src/tower/money_tower.hpp
+++ src/tower/money_tower.hpp
@@ -4,8 +4,9 @@ class MoneyTower : public Tower {
  public:
   MoneyTower(int x, int y, float size, int price,
              const std::string& texturename = "sprites/money_tower.png",
-             int money_per_wave = 300);
+             int money_per_wave = 200);
   void Upgrade();
+
 
  private:
   int money_per_wave_;
